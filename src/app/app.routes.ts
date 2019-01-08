e28f4dd1ac0fafcc5e9c40eab65864a1224a39d4
Paulo Eduardo Félix Teodoro <paulo.silva@ctvoicer.com.br>
2019-01-08 09:02:54 -0200

    aula rotas filhos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 export const ROUTES: Routes = [
     //quando nao epceficar nada ou vazia quero que meu componente direcione para pagina home
@@ -11,5 +13,13 @@ export const ROUTES: Routes = [
     {path: 'about', component: AboutComponent},
     {path: 'home', component: HomeComponent},
     {path: 'restaurants', component: RestaurantsComponent},
-    {path: 'restaurants/:id', component: RestaurantDetailComponent}
+    {path: 'restaurants/:id', component: RestaurantDetailComponent,
+    //so sera acionado se eu por restaurants/id/menu ou restaurants/id/reviews; 
+    children:[
+        //se eu não passar nenhum caminho, por padrao quero que redirecione para o menu exatamento '' se colocar qualquer coisa eue ja nao vai redirecionar 'full'
+        {path: '', redirectTo: 'menu', pathMatch: 'full'},
+        {path: 'menu', component: MenuComponent},
+        {path: 'reviews', component: ReviewsComponent}  
+    ]
+    }
 ]
