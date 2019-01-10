048ca7400e13f8e2a4e9d3f77964f2446bff52af
Paulo Eduardo Félix Teodoro <paulo.silva@ctvoicer.com.br>
2019-01-10 18:32:10 -0200

    adicionando formulario e trabalhando com validações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { OrderComponent } from './order/order.component';
 
 export const ROUTES: Routes = [
     //quando nao epceficar nada ou vazia quero que meu componente direcione para pagina home
     {path: '', component: HomeComponent},
     //Quando o modulo de roteamento encontrar o conteudo about quero que mostre o meu conteudo da pasta about
     {path: 'about', component: AboutComponent},
+    {path: 'order', component: OrderComponent},
     {path: 'home', component: HomeComponent},
     {path: 'restaurants', component: RestaurantsComponent},
     {path: 'restaurants/:id', component: RestaurantDetailComponent,
