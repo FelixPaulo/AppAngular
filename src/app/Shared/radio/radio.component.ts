28b77814048cfe532ffc047ce7cf35618cb8282c
Paulo Eduardo Félix Teodoro <paulo.silva@ctvoicer.com.br>
2019-01-15 17:51:16 -0200

    adicionando order-items da tabela

--- src/app/Shared/radio/radio.component.ts
+++ src/app/Shared/radio/radio.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import {NG_VALUE_ACCESSOR ,ControlValueAccessor} from '@angular/forms';
 import { RadioOption } from './radio-option.model';
 
 @Component({
   selector: 'mt-radio',
   templateUrl: './radio.component.html',
+  providers: [{provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => RadioComponent), multi: true}],
   styleUrls: ['./radio.component.css']
 })
-export class RadioComponent implements OnInit {
-
+export class RadioComponent implements OnInit, ControlValueAccessor {
+  
   @Input() options: RadioOption[];
   value: any;
+  onChange: any;
 
   constructor() { }
 
@@ -18,5 +21,27 @@ export class RadioComponent implements OnInit {
 
   setValue(value: any){
     this.value = value;
+    this.onChange(this.value);
   }
+
+  //Vai ser chamado pela diretivas quando querem passar um valor para o seu componente
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  //tem que chamar a função recebita por parametro sempre que o valor interno do component mudar
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  //Registrar que o usuario entrou no componente
+  registerOnTouched(fn: any): void {
+    // throw new Error("Method not implemented.");
+  }
+
+  //
+  setDisabledState?(isDisabled: boolean): void {
+    // throw new Error("Method not implemented.");
+  }
+
 }
