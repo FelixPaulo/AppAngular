391d56830157f8a11d2d1499c83e655a78275034
Paulo Eduardo Félix Teodoro <paulo.silva@ctvoicer.com.br>
2019-01-24 17:26:08 -0200

    organizando modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
+import { RouterModule, PreloadAllModules } from '@angular/router';
 // importar routes para as rotas
 import{ROUTES} from './app.routes';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-// import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
-// import { RestaurantsServices } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { SharedModule } from './Shared/shared.module';
+// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 // import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 // import { OrderComponent } from './order/order.component';
 // import { InputComponent } from './Shared/input/input.component';
@@ -26,10 +24,10 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 // import { OrderItemsComponent } from './order/order-items/order-items.component';
 // import { OrderService } from './order/order.service';
 // import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { SharedModule } from './Shared/shared.module';
+// import { RestaurantsServices } from './restaurants/restaurants.service';
+// import { AboutComponent } from './about/about.component';
 // import { RatingComponent } from './Shared/rating/rating.component';
-import {CoreModule} from './core/core.module';
+// import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -59,12 +57,12 @@ import {CoreModule} from './core/core.module';
     //Esses dois ja estao sendo importados no shared.module.ts
     // FormsModule,
     // ReactiveFormsModule,
-    SharedModule,
-    CoreModule,
+    SharedModule.forRoot(),
+    // CoreModule,
     //Devo importar minhas rotas definidas no caso o ROUTES
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
-  
+
   //sempre que alguem pedir esse LOCALE_ID esse cara ira retornar o valor pt-BR
   // providers: [RestaurantsServices, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
