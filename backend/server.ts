0ea5cc1d3e81ab200bfb9870a28357f3fcf1466c
Paulo Eduardo Félix Teodoro <paulo.silva@ctvoicer.com.br>
2019-02-27 14:23:57 -0300

    backend request

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,8 @@ import {Express} from 'express'
 import * as fs from 'fs'
 import * as https from 'https'
 
+import {handleAuthentication} from './auth'
+
 const server: Express = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
@@ -15,17 +17,17 @@ server.use(middlewares)
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 
+//Middleware para login
+server.post('/login', handleAuthentication);
+
+//Use default router
+server.use(router)
+
 const options = {
   cert: fs.readFileSync('./backend/keys/cert.pem'),
   key: fs.readFileSync('./backend/keys/key.pem')
 }
 
-https.createServer(options, server).listen(3002, () => {
+https.createServer(options, server).listen(3001, () => {
   console.log('JSON Server is running on https://localhost:3001')
 })
-
-// // Use default router
-// server.use(router)
-// server.listen(3000, () => {
-//   console.log('JSON Server is running')
-// })
